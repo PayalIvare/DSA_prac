c8ed3acbe545ef067fc09c1b6341bfc33b5a755e
Payal Ivare <78032235+PayalIvare@users.noreply.github.com>
2024-05-08 07:57:31 +0530

    Update F24.cpp

--- F24.cpp
+++ F24.cpp
@@ -1,194 +1,288 @@
-#include<iostream>
-#include<fstream>
-#include<string.h>
+#include <string.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
 using namespace std;
-typedef struct stud
-	{
-		int roll;
-		char name[10];
-		char div;
-		char add[10];
-		
-	};
-class student
-  {
-  	public:
-	
-	 stud rec;
-	
-	  void insert(stud rec1);
-	  void display();
-	  int search();
-	  void Delete();
-	  stud getdata();
-	  
-  };
-  stud student::getdata()
-  {
-  	
-  	 cout<<"\n Enter roll no: ";
-  	 cin>>rec.roll;
-  	 cout<<"\n Enter name: ";
-  	 cin>>rec.name;
-  	 cout<<"\n Enter div: ";
-  	 cin>>rec.div;
-  	 cout<<"\n Enter add: ";
-  	 cin>>rec.add;
-  	 
-  	 return rec;
-  	 
-  }
-void student::insert(stud rec1)
-  {
-	stud rec;
-	int  i,n,k;
-	
-	fstream f;
-	f.open("stud.dat",ios::out|ios::in|ios::binary); 
-	//rec1.status=0; 
-	f.seekg(0,ios::end);
-	
-	n = (f.tellg()/sizeof(stud));
-	
-	
-	if(n == 0)
-	{
-		f.write((char *)&rec1,sizeof(stud))<<flush;
-		
-        f.close();
-        return ; 
-	}
-	i=n-1;
-	
-	
-	while(i>=0)
-	{
-		
-		f.seekg(i*sizeof(stud),ios::beg);
-		f.read((char *)&rec,sizeof(stud));
-		
-		
-		
-		if(rec.roll>rec1.roll)
-		{
-			f.seekp(i+1*sizeof(stud),ios::beg);
-			f.write((char *)&rec,sizeof(stud))<<flush;
-		}
-		else
-		{
-			break;
-		}
-		i--;
-	}
-	
-	i++;
-	f.seekp(i*sizeof(stud),ios::beg);
-	f.write((char *)&rec1,sizeof(stud))<<flush;
-	f.close();
-	
-	}
-void student::display()
-  {
-     stud rec;
-     int i=1 , n;
-     fstream f;
-     f.open("stud.dat",ios::out|ios::in|ios::binary);
-     f.seekg(0,ios::end);
-     n=(f.tellg()/sizeof(stud));
-     f.seekg(0,ios::beg);
-     for(i=1 ; i<=n ; i++)
-     {
-     	f.read((char *)&rec,sizeof(stud));
-     	cout<<"\n"<<rec.roll<<"\t"<<rec.name<<"\t"<<rec.div<<"\t"<<rec.add;
-	 }
-	 f.close();
-  }
-int student::search()
-  {
-	 int r,i=0;
-	ifstream fin;
-	fin.open("stud.dat",ios::in|ios::binary);
-	fin.seekg(0,ios::beg);
-	cout<<"\n\tEnter a Roll No: ";
-	cin>>r;
-	while(fin.read((char *)&rec,sizeof(stud)))
-	  {
-		if(rec.roll==r)
-		  {
-			cout<<"\n\tRecord Found...\n";
-			cout<<"\n\tRoll\tName\tDiv\tAddress";
-			cout<<"\n\t"<<rec.roll<<"\t"<<rec.name<<"\t"<<rec.div<<"\t"<<rec.add;
-			return i+1;
-		  }
-		i++;
-	  }
-	fin.close();
-	return 0;
-  }
-void student::Delete()
-  {
-    int roll;
-    cout <<"\nEnter Roll No to delete: ";
-    cin >>roll;
-    ifstream fin;
-    ofstream fout;
-	fin.open("stud.dat", ios::in|ios::binary); 
-	fout.open("temp.dat", ios::out|ios::binary); // Temporary file for storing non-deleted records
-    while (fin.read((char*)&rec, sizeof(stud))) {
-        if (rec.roll != roll) {
-            fout.write((char*)&rec, sizeof(stud));
+
+typedef struct employee {
+    int empId;
+    char name[50];
+    char designation[50];
+    int salary;
+} employee;
+
+typedef struct index_pair {
+    int key;
+    int position;
+} index_pair;
+
+class employeeDatabase {
+    string data_file_name = "ind_employee_data.dat";
+    string index_file_name = "ind_employee_index.dat";
+   public:
+    employeeDatabase();
+    void addEmployee(int eid, char name[50], char dest[50], int sal);
+    void searchEmployee(int);
+    void deleteEmployee(int);
+    bool isPresent(int);
+    void display_all();
+    employee readEmp(int pos);
+};
+
+employee employeeDatabase::readEmp(int pos) {
+    fstream data_file(data_file_name, ios::binary | ios::in | ios::ate);
+    employee emp;
+    if (pos >= data_file.tellg() || pos == -1) {
+        emp.empId = -1;
+        return emp;
+    }
+    data_file.seekg(pos, ios::beg);
+    data_file.read((char *)&emp, sizeof(emp));
+    data_file.close();
+    return emp;
+}
+
+employeeDatabase::employeeDatabase() {
+    fstream data_file(data_file_name);
+    fstream index_file(index_file_name);
+    if (data_file.fail() | index_file.fail()) {
+        cout << "Created a new file." << endl;
+        data_file.open(data_file_name, ios::binary | ios::out | ios::app);
+        index_file.open(index_file_name, ios::binary | ios::out | ios::app);
+    } else {
+        cout << "File already exists." << endl;
+    }
+    data_file.close();
+    index_file.close();
+}
+
+void employeeDatabase::addEmployee(int eid, char name[50], char dest[50], int sal) {
+    fstream data_file(data_file_name, ios::binary | ios::out | ios::app);
+    fstream index_file(index_file_name, ios::binary | ios::in);
+    index_pair current, temp;
+    char *all_data;
+    employee emp;
+    int pos, ipos;
+    bool update = false;
+
+    while (index_file.read((char *)&current, sizeof(index_pair))) {
+      
+        if (current.key == eid) {
+            if (current.position == -1) {
+                ipos = (int)index_file.tellg() - sizeof(current);
+                update = true;
+                break;
+            } else {
+                cout << "Cannot add employee, already exists."
+                     << endl;
+                goto exit_addEmployee;
+            }
         }
     }
-    fin.close();
-    fout.close();
+    index_file.close();
 
-    // Replace students.dat with temp.dat
-    remove("stud.dat");
-    rename("temp.dat", "stud.dat");
-    cout<<"\n Record Deleted Successfully ";
-  }
-	
-int main()
-  {
-	student obj;
-	int ch,key;
-	char ans;
-	stud record;
-	
-	ofstream fout;
-	fout.open("stud.dat",ios::out|ios::binary); 
-	fout.close();
-	fout.open("temp.dat",ios::out|ios::binary); 
-	fout.close();
-	
-	do
-	  {
-	  	cout<<"\n";
-		cout<<"\n\t** Student Information **";
-		cout<<"\n\t1. Create\n\t2. Display\n\t3. Delete\n\t4. Search\n\t5. Exit";
-		cout<<"\n\t..... Enter Your Choice: ";
-		cin>>ch;
-		switch(ch)
-		  {
-		  	
-			case 1: 
-			 record = obj.getdata();
-			 
-			obj.insert(record);
-				break;
-			case 2:	obj.display();
-				break;
-			case 3: obj.Delete();
-				break;
-			case 4: key=obj.search();
-				if(key==0)
-				  cout<<"\n\tRecord Not Found...\n";
-				break;
-			case 5:
-				cout<<"\n Program Ended Successfully ";
-				break;
-		  }
-		
-	  }while(ch!=5);
-return 1;
-  }
+    emp.empId = eid;
+    strcpy(emp.name, name);
+    strcpy(emp.designation, dest);
+    emp.salary = sal;
+
+    data_file.seekp(0, ios::end);
+    pos = data_file.tellp();
+    data_file.write((char *)&emp, sizeof(emp));
+    current.key = eid;
+    current.position = pos;
+    // cout << pos << endl;
+    if (update) {
+        index_file.open(index_file_name, ios::binary | ios::out);
+        index_file.seekp(ipos, ios::beg);
+        index_file.write((char *)&current, sizeof(current));
+    } else {
+        bool written = false;
+        // inserting record in sorted order
+        vector<index_pair> index_pairs;
+        index_file.open(index_file_name, ios::binary | ios::in);
+        while (index_file.read((char *)&temp, sizeof(index_pair))) {
+            if (!written and temp.key > eid) {
+                written = true;
+                index_pairs.push_back(current);
+            }
+            index_pairs.push_back(temp);
+        }
+        if (!written) {
+            index_pairs.push_back(current);
+        }
+
+        index_file.clear();
+        index_file.close();
+
+        // write records
+        index_file.open(index_file_name, ios::binary | ios::out);
+
+        for (int i = 0; i < index_pairs.size(); i++) {
+            current = index_pairs[i];
+            if (current.position != -1) {
+                index_file.write((char *)&current, sizeof(index_pair));
+            }
+        }
+    }
+    cout << "Employee added successfully." << endl;
+// close and exit
+exit_addEmployee:
+    data_file.close();
+    index_file.close();
+}
+
+void display_emp(employee emp) {
+    cout << "ID:\t" << emp.empId << endl;
+    cout << "Name:\t" << emp.name << endl;
+    cout << "Designation:\t" << emp.designation << endl;
+    cout << "Salary:\tRs. " << emp.salary << endl;
+}
+
+void employeeDatabase::searchEmployee(int eid) {
+    fstream index_file(index_file_name, ios::binary | ios::in);
+    index_pair current;
+    int pos = -1;
+
+    while (index_file.read((char *)&current, sizeof(index_pair))) {
+        if (current.key == eid) {
+            pos = current.position;
+            break;
+        }
+    }
+
+    employee emp = readEmp(pos);
+    if (emp.empId == -1) {
+        cout << "Employee does not exist." << endl;
+    } else {
+        cout << "--- EMPLOYEE FOUND ---" << endl;
+        display_emp(emp);
+        cout << "--- END OF RECORD ---" << endl;
+    }
+
+    index_file.close();
+}
+
+bool employeeDatabase::isPresent(int eid) {
+    fstream index_file(index_file_name, ios::binary | ios::in);
+    index_pair current;
+
+    while (index_file.read((char *)&current, sizeof(index_pair))) {
+        if (current.key == eid) {
+            if (current.position == -1) {
+                index_file.close();
+                return false;
+            } else {
+                index_file.close();
+                return true;
+            }
+        }
+    }
+    index_file.close();
+    return false;
+}
+
+void employeeDatabase::deleteEmployee(int eid) {
+    fstream index_file(index_file_name, ios::binary | ios::in);
+    index_pair current; 
+    bool deleted = false;
+    vector<index_pair> pairs;
+
+    while (index_file.read((char *)&current, sizeof(index_pair))) {
+        if (current.key == eid) {
+            deleted = true;
+        } else {
+            pairs.push_back(current);
+        }
+    }
+    index_file.close();
+    if (deleted) {
+        index_file.open(index_file_name, ios::binary | ios::out);
+        index_file.clear();
+        index_file.seekp(0, ios::beg);
+
+        for (int i = 0; i < pairs.size(); i++) {
+            current = pairs[i];
+            index_file.write((char *)&current, sizeof(index_pair));
+        }
+        index_file.close();
+        cout << "Employee removed from record." << endl;
+    } else {
+        cout << "Employee does not exist in the record." << endl;
+    }
+}
+
+void employeeDatabase::display_all() {
+    fstream index_file(index_file_name, ios::binary | ios::in);
+    fstream data_file(data_file_name, ios::binary | ios::in);
+    index_pair current;
+    employee emp;
+    while (index_file.read((char *)&current, sizeof(index_pair))) {
+        if (current.position != -1) {
+            data_file.seekg(current.position, ios::beg);
+            data_file.read((char *)&emp, sizeof(emp));
+            cout << emp.empId << " | " << emp.name << " | " << emp.designation
+                 << " | " << emp.salary << endl;
+        }
+    }
+    index_file.close();
+    data_file.close();
+}
+
+int main() {
+    employeeDatabase db;
+    int eid, sal;
+    char name[50], dest[50];
+    int ch;
+    do {
+        cout << endl << "--- MAIN MENU ---" << endl;
+        cout << "1 -> Add employee" << endl;
+        cout << "2 -> Search employee" << endl;
+        cout << "3 -> Delete employee" << endl;
+        cout << "4 -> Display all" << endl;
+        cout << "5 -> Exit" << endl;
+        cout << "Choose an option (1-5):\t";
+        cin >> ch;
+        switch (ch) {
+            case 1:
+            reenter_eid:
+                cout << "Employee ID:\t";
+                cin >> eid;
+                if (db.isPresent(eid)) {
+                    cout << "Employee already exists in record. Please try again."
+                         << endl;
+                    goto reenter_eid;
+                }
+                cout << "Name:\t";
+                cin >> name;
+                cout << "Designation:\t";
+                cin >> dest;
+                cout << "Salary:\tRs. ";
+                cin >> sal;
+
+                db.addEmployee(eid, name, dest, sal);
+                break;
+            case 2:
+                cout << "Employee ID:\t";
+                cin >> eid;
+                db.searchEmployee(eid);
+                break;
+            case 3:
+                cout << "Employee ID:\t";
+                cin >> eid;
+                db.deleteEmployee(eid);
+                break;
+            case 4:
+                db.display_all();
+                break;
+            case 5:
+                cout << "\n\n// END OF CODE\n\n" << endl;
+                break;
+            default:
+                cout << "Please choose a valid option (1-5)." << endl;
+                break;
+        }
+    } while (ch != 5);
+
+    return 0;
+}
